b429182b6f57760bcbbbc81d16a9b5c9591dda79
Mahaayuga <benoit.mazure@gmail.com>
2017-11-09 21:35:51 +0100

    modif mineures

--- app/tests.rb
+++ app/tests.rb
@@ -7,19 +7,21 @@ poi.mise_en_memoire("../db/db_poi_test.csv")
 #poi.liste
 
 #Test de récupération d'un text
-ghostkey = IO.read("./public/css/map.css")
-puts ghostkey
+#ghostkey = IO.read("./public/css/map.css")
+#puts ghostkey
 
-#test d'un calcul de moyenne
+#test d'un calcul de moyenne  ## IT WORKS
 #mescoord = [50, 48, 51, 45]
-#p poi.gravite(mescoord)
+#puts poi.gravite(mescoord)
 
 #origin = {lat: 48.938127, lng: 2.008509}
 #p origin[:lat] , origin[:lng]
 
-#test collecte des points
-#run = [0,1,3]
-#puts poi.monparcours(run)
+#test collecte des points  ## IT COULD WORK
+run = 0.upto(5).to_a
+puts poi.monparcours(run)
+print "\n"
+puts poi.moncadre(run)
 
 
 #print poi.coordonnees[0]
@@ -31,5 +33,8 @@ puts ghostkey
 #print poi.coordonnees
 #print poi.coordonnees.class
 
+#poi.coordonnees.count.times do |i|
+#  print "#{poi.coordonnees[i][1].to_f} \n"
+#end
 
 print "\n"
